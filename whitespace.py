03a0555600dba357b05641b0e19384fe401700bf
Jamshid93 <hamzayevjf@gmail.com>
2020-04-19 16:33:08 +0500

    Python adding whitespace

---
+++ whitespace.py
@@ -0,0 +1,6 @@
+# bu yerda qanday qilib print qilgan ma'lumotimizni bo'sh joy orqali yoki yangi qatordan chiqarishni ko'rib o'tamiz
+# bu whitespace deb yuritiladi  
+print("Hello!Howareyou.")
+print("Hello!\tHow\tare\tyou.")# bu ya'ni ma'lumotlar orasiga bo'sh joy qo'shish \t orqali amalga oshiriladi
+print("Hello!\nHow\nare\nyou.") # bu ya'ni ma'lumotni xar birini yangi qatordan chiqarish \n orqali amalga oshiriladi.
+print("Hello!\nHow\tare\tyou.")
