78e21645a80fa31918471e4d8b246b8d6f2e9e4d
Jamshid93 <hamzayevjf@gmail.com>
2020-04-23 17:07:23 +0500

    Python List

---
+++ list.py
@@ -0,0 +1,8 @@
+# bu yerda Python da listlar xaqida : List degani bu bir qancha ma'lumotlarni bir joyda to'plangani
+# bu xox raqam ko'rinishida xox so'z ko'rinishida bo'lishi mumkin.
+# listni kvadrat qavsga [ ] olish bilan e'lon qilamiz va ma'lumotlarni bir birida vergul bilan jratamiz listni ichidagi
+name=["Jhon","jamshid","Alisher","Maks"]
+print(name)
+print(name[0]) # bu yerda [0] degani index sini ko'rsatadi ya'ni 0 chi tartibda listda qaysi ma'lumot turganini bizda "Jhon" turibdi
+print(name[1].title()) # bu title() methodini andqy qilib qo'lash uchun
+print(name[-1])  # bu [-1]  degani listni oxiridagi elementini chiqar degani
---
+++ list1.py
@@ -0,0 +1,4 @@
+# bu yerda litni ichidagi alohida lemetiga murajat qilishni ko'ramiz
+car=["BMW","NEXIA","Tiko","Damas","Ford"]
+massage=f"My firs car was {car[2]}! " # bu yerda fstringdan foydalanayapmiz.
+print(massage)
