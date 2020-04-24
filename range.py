880ca90f82d62e6ec431cc7aea7a015edb919a91
Jamshid93 <hamzayevjf@gmail.com>
2020-04-24 18:44:44 +0500

    range function

---
+++ range.py
@@ -0,0 +1,4 @@
+# bu yerda range() funvtion haqida gaplashamiz bu degani shu for loop uchun raqamlar bilan ishlashdagi chegarani ko'rsatadi
+numbers=[]
+for value in range(1,6): # bu degani menag 1-5 gacha raqamlarni chiqar degani 6 raqamini o'zi kirmaydi bunga 
+    print(value)
